7ff40698c2ee99f6176fcdc8b306c4a7d577621c
DianaBaRo <33879699+DianaBaRo@users.noreply.github.com>
2018-11-07 12:45:57 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Lower row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Rigth column
+  [0,4,8], # Left to Rigth diagonal
+  [2,4,6] # Rigth to Left diagonal
+]
+
 def display_board (board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
