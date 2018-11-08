1f513efacb984d7601b3100abd51b4adc98b9337
DianaBaRo <33879699+DianaBaRo@users.noreply.github.com>
2018-11-08 13:20:19 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+puts "Welcome to Tic Tac Toe!"
+display_board(board)
+
+play(board)
+
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
